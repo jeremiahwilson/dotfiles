16b22ad55df9d601d479262b18feaeb370ef9bf6
Jeremiah Wilson <jewil@pm.me>
2025-04-27 16:11:58 -0400

    Added yazi

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -8,10 +8,13 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    indent = { enabled = true },
     input = { enabled = true },
     quickfile = { enabled = true },
     scroll = { enabled = true },
     words = { enabled = true },
+    indent = {
+      enabled = true,
+      animate = { enabled = false }
+    },
   },
 }
