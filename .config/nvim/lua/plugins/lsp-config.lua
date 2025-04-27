30d6f107b520326ba69f9c13e3fc05c41804cbb2
Jeremiah Wilson <jewil@pm.me>
2025-04-27 15:12:39 -0400

    Yazi and mason tweaks

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -8,7 +8,9 @@ return {
     },
     { -- tell mason which lsps to install
         "williamboman/mason-lspconfig.nvim",
-
+        opts = {
+          auto_install = true,
+        },
         config = function()
             require('mason-lspconfig').setup({
                 ensure_installed = { "lua_ls", "ts_ls", "eslint", "html" }
